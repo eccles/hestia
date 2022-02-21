297a620e66e1ab1b1b1f30c7680742fc38d401e8
Paul Hewlett <paul.hewlett@jitsuin.com>
2022-02-21 08:27:40 +0000

    Installation scripts have common code
    Problem:
    Install scripts are difficult to modify because of
    repeated code.
    
    Solution:
    Refactor and use a common set of functions.
    
    Signed-off-by: Paul Hewlett <phewlett76@gmail.com>

--- scripts/tools/install.sh
+++ scripts/tools/install.sh
@@ -2,21 +2,10 @@
 #
 # Installs necessary tools
 #
-. ./scripts/source/log
-. ./scripts/source/environment
-. ./scripts/tools/source/golang
-. ./scripts/tools/source/golangci-lint
-. ./scripts/tools/source/protoc
-. ./scripts/tools/source/mockery
-. ./scripts/tools/source/protoc-gen-go
-. ./scripts/tools/source/protoc-gen-go-grpc
-. ./scripts/tools/source/protoc-gen-validate
-
-export GOBIN=$HOME/go/bin
-golang_install
-golang_lint_install
-mockery_install
-protoc_install
-protoc_gen_go_install
-protoc_gen_go_grpc_install
-protoc_gen_validate_install
+(. ./scripts/tools/source/go && go_install)
+(. ./scripts/tools/source/golangci-lint && golangci-lint_install)
+(. ./scripts/tools/source/protoc && protoc_install)
+(. ./scripts/tools/source/mockery && mockery_install)
+(. ./scripts/tools/source/protoc-gen-go && protoc-gen-go_install)
+(. ./scripts/tools/source/protoc-gen-go-grpc && protoc-gen-go-grpc_install)
+(. ./scripts/tools/source/protoc-gen-validate && protoc-gen-validate_install)
