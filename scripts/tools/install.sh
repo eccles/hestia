684012a1610e537d482e551bfc0740db2f246b14
Paul Hewlett <phewlett76@gmail.com>
2023-05-20 09:59:11 +0100

    Install nvm command
    Signed-off-by: Paul Hewlett <phewlett76@gmail.com>

--- scripts/tools/install.sh
+++ scripts/tools/install.sh
@@ -2,10 +2,7 @@
 #
 # Installs necessary tools
 #
-(. ./scripts/tools/source/go && go_install)
-(. ./scripts/tools/source/golangci-lint && golangci-lint_install)
-(. ./scripts/tools/source/protoc && protoc_install)
-(. ./scripts/tools/source/mockery && mockery_install)
-(. ./scripts/tools/source/protoc-gen-go && protoc-gen-go_install)
-(. ./scripts/tools/source/protoc-gen-go-grpc && protoc-gen-go-grpc_install)
-(. ./scripts/tools/source/protoc-gen-validate && protoc-gen-validate_install)
+for f in go golangci-lint protoc mockery protoc-gen-go protoc-gen-go-grpc protoc-gen-validate nvm
+do
+	(. ./scripts/tools/source/${f} && ${f}_install)
+done
--- scripts/tools/which.sh
+++ scripts/tools/which.sh
@@ -2,10 +2,8 @@
 #
 # Finds installed tools
 #
-(. ./scripts/tools/source/go && go_which)
-(. ./scripts/tools/source/golangci-lint && golangci-lint_which)
-(. ./scripts/tools/source/protoc && protoc_which)
-(. ./scripts/tools/source/mockery && mockery_which)
-(. ./scripts/tools/source/protoc-gen-go && protoc-gen-go_which)
-(. ./scripts/tools/source/protoc-gen-go-grpc && protoc-gen-go-grpc_which)
-(. ./scripts/tools/source/protoc-gen-validate && protoc-gen-validate_which)
+
+for f in go golangci-lint protoc mockery protoc-gen-go protoc-gen-go-grpc protoc-gen-validate nvm
+do
+        (. ./scripts/tools/source/${f} && ${f}_which)
+done
