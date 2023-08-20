9e74ba32a3a2d75506c4eb9ac8068e2c518f19d5
Paul Hewlett <phewlett76@gmail.com>
2023-08-20 09:39:53 +0100

    Stop generating own go code

--- pkg/widgets/service/generate.go
+++ pkg/widgets/service/generate.go
@@ -1,6 +1,3 @@
 package widgetsService
 
-// Generate boilerplate for grpcserver.go and service.go
-//go:generate ../../../scripts/grpcserver.sh  widgets
-
 //go:generate mockery --all --dry-run=false
